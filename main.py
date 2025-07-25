9d6c5211616ac0e0b699f776de969106126e15ab
Andriy900 <andrukhov9002@gmail.com>
2025-07-25 13:07:16 +0300

    Added send quoates

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 # Replace 'YOUR_TOKEN' with your actual bot token
-TOKEN = 'YOUR_TOKEN'
+TOKEN = '7753729694:AAGZsZeqgN_E7jBSM05oYU8xqQ9QhX8AH8E'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello! I am your bot.')
 
-async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Here is your quote: "The best way to get started is to quit talking and begin doing."')
+async def quote(update: Update, context: ContextTypes.DEFAULT_TYPE, data):
+    await update.message.reply_text(f"Привіт, твоя цитата на сьогодні: {data["quote"]} - {data["author"]}")
+
+from functools import partial
 
 if __name__ == '__main__':
+    data = {"quote": "Цитата",
+         "author": "Андрій"}
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start))
-    app.add_handler(CommandHandler('quote', quote))
+    app.add_handler(CommandHandler('quote', partial(quote, data = data)))
     app.run_polling()
